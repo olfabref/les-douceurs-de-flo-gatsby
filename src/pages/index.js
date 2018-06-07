f309773c716a42380ebac4402cf13601d5a023f6
Olivier FABRE <fabre.o@mipih.fr>
2018-06-07 14:38:06 +0200

    Use react-bulma-components instead of bulma only

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,108 +1,111 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import Logo from "../components/Logo"
-import Slider from "react-slick"
+import React from "react";
+import PropTypes from "prop-types";
+import Logo from "../components/Logo";
+import Slider from "react-slick";
+import { Columns, Container, Heading, Hero, Section } from "react-bulma-components";
 
 export default class IndexPage extends React.Component {
-    render() {
-        const {data} = this.props
-        const {edges: posts} = data.allMarkdownRemark
-        var settings = {
-            dots: true,
-            infinite: true,
-            speed: 500,
-            slidesToShow: 1,
-            slidesToScroll: 1
-        };
+  render() {
+    const { data } = this.props;
+    const { edges: posts } = data.allMarkdownRemark;
+    var settings = {
+      dots: true,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 1,
+      slidesToScroll: 1
+    };
 
-        return (
-            <div>
-                <section className="section hero is-fullheight is-dark is-bold">
-                    <div className="hero-body">
-                        <div className="container">
-                            <div className="columns">
-                                <div className="column is-8 is-offset-2 has-text-centered">
-                                    <h1 className="title"><Logo/></h1>
-                                    <h2>Pâtisseries, Cake Design, and Co.</h2>
-                                    <div className="content">
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+    return (
+      <div>
+        <Hero size="fullheight" color="dark" gradient>
+          <Hero.Body>
+            <Container className="has-text-centered">
+              <Heading>
+                <Logo/>
+              </Heading>
+              <Heading subtitle size={3}>
+                Pâtisseries, Cake Design, and Co.
+              </Heading>
+            </Container>
+          </Hero.Body>
+        </Hero>
+        <Hero size="fullheight" color="dark" gradient>
+          <Hero.Body>
+            <Container>
+              <Columns>
+                <Columns.Column size="half">
+                  <Slider {...settings}>
+                    <div>
+                      <figure className="image">
+                        <img src="/img/2.jpg"/>
+                      </figure>
                     </div>
-                </section>
-                <section className="section columns">
-                    <div className="column is-half">
-                        <Slider {...settings}>
-                            <div>
-                                <figure className="image">
-                                    <img src="/img/2.jpg"/>
-                                </figure>
-                            </div>
-                            <div>
-                                <figure className="image">
-                                    <img src="/img/3.jpg"/>
-                                </figure>
-                            </div>
-                            <div>
-                                <figure className="image">
-                                    <img src="/img/5.jpg"/>
-                                </figure>
-                            </div>
-                            <div>
-                                <figure className="image">
-                                    <img src="/img/6.jpg"/>
-                                </figure>
-                            </div>
-                        </Slider>
+                    <div>
+                      <figure className="image">
+                        <img src="/img/3.jpg"/>
+                      </figure>
                     </div>
-                    <div className="column is-half">
-                        <div className="hero is-halfheight">
-                            <div className="hero-body">
-                                <div className="container has-text-left">
-                                    <h1 className="title">Cake design</h1>
-                                </div>
-                            </div>
-                        </div>
+                    <div>
+                      <figure className="image">
+                        <img src="/img/5.jpg"/>
+                      </figure>
                     </div>
-                </section>
-                <section className="section columns">
-                    <div className="column is-half">
-                        <div className="hero is-halfheight">
-                            <div className="hero-body">
-                                <div className="container has-text-right">
-                                    <h1 className="title">Pâtisseries</h1>
-                                </div>
-                            </div>
-                        </div>
+                    <div>
+                      <figure className="image">
+                        <img src="/img/6.jpg"/>
+                      </figure>
                     </div>
-                    <div className="column is-half">
-                        <Slider {...settings}>
-                            <div>
-                                <figure className="image">
-                                    <img src="/img/1.jpg"/>
-                                </figure>
-                            </div>
-                            <div>
-                                <figure className="image">
-                                    <img src="/img/4.jpg"/>
-                                </figure>
-                            </div>
-                        </Slider>
+                  </Slider>
+                </Columns.Column>
+                <Columns.Column size="half">
+                  <Section>
+                    <h1 className="title">Cake design</h1>
+                  </Section>
+                </Columns.Column>
+              </Columns>
+            </Container>
+          </Hero.Body>
+        </Hero>
+        <Hero size="fullheight" color="dark" gradient>
+          <Hero.Body>
+            <Container>
+              <Columns>
+                <Columns.Column size="half">
+                  <Section>
+                    <h1 className="title">Pâtisseries</h1>
+                  </Section>
+                </Columns.Column>
+                <Columns.Column size="half">
+                  <Slider {...settings}>
+                    <div>
+                      <figure className="image">
+                        <img src="/img/1.jpg"/>
+                      </figure>
                     </div>
-                </section>
-            </div>
-        )
-    }
+                    <div>
+                      <figure className="image">
+                        <img src="/img/4.jpg"/>
+                      </figure>
+                    </div>
+                  </Slider>
+                </Columns.Column>
+              </Columns>
+            </Container>
+          </Hero.Body>
+        </Hero>
+      </div>
+    );
+  }
 }
 
 IndexPage.propTypes = {
-    data: PropTypes.shape({
-        allMarkdownRemark: PropTypes.shape({
-            edges: PropTypes.array,
-        }),
-    }),
-}
+  data: PropTypes.shape({
+    allMarkdownRemark: PropTypes.shape({
+      edges: PropTypes.array
+    })
+  })
+};
 
 export const pageQuery = graphql`
   query IndexQuery {
@@ -126,4 +129,4 @@ export const pageQuery = graphql`
       }
     }
   }
-`
+`;
